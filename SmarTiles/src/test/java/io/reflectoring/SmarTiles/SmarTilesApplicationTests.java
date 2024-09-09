596e2a54752cba2ee5e9deed11df3e5b65f5f28a
Pedro Ferreira Martins <100172927+Pedro-FMartins@users.noreply.github.com>
2024-09-09 16:01:14 -0300

    Arquivos iniciais do projeto

---
+++ SmarTiles/src/test/java/io/reflectoring/SmarTiles/SmarTilesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.reflectoring.SmarTiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarTilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ live-crud-java-spring-initial-template/src/main/java/com/example/crud/domain/tile/RequestTile.java
@@ -0,0 +1,4 @@
+package com.example.crud.domain.tile;
+
+public record RequestTile(float height, float width, float gap, float depth) {
+}
